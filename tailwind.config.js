c77c5d27015e1bca6bc69770ff8c39a142af9c3e
eren.emmez <eren.emmez@enuygun.com>
2022-12-23 09:27:05 +0300

    style: tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "pp-pale-gray": "#DDDDDD",
+        "pp-main-green": "#265A5C",
+        "pp-secondary-green": "#09766F",
+        "pp-danger": "#FF5A5E",
+        "pp-warning": "#E1AA52",
+        "pp-text": "#52575D",
+        "pp-title": "#41444B",
+        "pp-skypale-green": "#F1F1F1",
+      },
+    },
   },
   plugins: [],
 };

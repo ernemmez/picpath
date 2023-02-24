ec50b501c873225bbeb2d886d16136ec826349b2
eren.emmez <eren.emmez@enuygun.com>
2023-02-24 23:49:30 +0300

    refactor: middleware redirects

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
-export { default } from "next-auth/middleware";
-
+import { getSession } from "next-auth/react";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
+
   if (path.startsWith("/login") || path.startsWith("/signin")) {
     return NextResponse.redirect(new URL("/auth?redirect=signin", request.url));
   }
@@ -12,10 +12,21 @@ export function middleware(request: NextRequest) {
   if (path.startsWith("/register") || path.startsWith("/signup")) {
     return NextResponse.redirect(new URL("/auth?redirect=signup", request.url));
   }
+
+  if (path === "/") {
+    const session = await getSession();
+    if (!session)
+      return NextResponse.redirect(
+        new URL("/auth?redirect=signin", request.url)
+      );
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
   matcher: [
+    "/",
     "/login/:path*",
     "/register/:path*",
     "/signin/:path*",

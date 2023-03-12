889e4642684604ebf646a28331325db7146ec3eb
eren.emmez <eren.emmez@enuygun.com>
2023-03-12 18:25:27 +0300

    feat: header urls

--- components/Header/DesktopHeader/index.tsx
+++ components/Header/DesktopHeader/index.tsx
@@ -1,6 +1,7 @@
 import Search from "components/Search";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
+import Link from "next/link";
 import CreateIcon from "public/icons/create-icon.svg";
 import ExploreIcon from "public/icons/explore-icon.svg";
 import type { FC } from "react";
@@ -11,12 +12,14 @@ const DesktopHeader: FC = () => {
     return (
         <div className="w-full border h-[59px] flex justify-between items-center px-9">
             <div className="w-5/12 flex justify-between items-center">
-                <Image
-                    src="/icons/favicons/180x180.png"
-                    width={40}
-                    height={40}
-                    alt="Picpath"
-                />
+                <Link href="/">
+                    <Image
+                        src="/icons/favicons/180x180.png"
+                        width={40}
+                        height={40}
+                        alt="Picpath"
+                    />
+                </Link>
                 <div className="w-[55%]">
                     <Search />
                 </div>
@@ -27,12 +30,12 @@ const DesktopHeader: FC = () => {
                     onClick={() => alert('add point and share photo || share photo in any point')}
                     className="cursor-pointer"
                 />
-                <ExploreIcon
-                    width="38px"
-                    onClick={() => alert('explore')}
-                    className="cursor-pointer"
-                />
-
+                <Link href="/explore">
+                    <ExploreIcon
+                        width="38px"
+                        className="cursor-pointer"
+                    />
+                </Link>
                 <UserArea user={{ username: data?.user?.username }} />
             </div>
         </div>
--- components/Header/MobileNavbar/index.tsx
+++ components/Header/MobileNavbar/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import CreateIcon from "public/icons/create-icon.svg";
 import ExploreIcon from "public/icons/explore-icon.svg";
 import HomepageIcon from "public/icons/mobile-homepage.svg";
@@ -8,16 +9,20 @@ const MobileNavbar: FC = () => {
 
     return (
         <div className="border max-h-[100px] w-full absolute bottom-0 flex justify-around items-center px-[60px] py-[12px]">
-            <HomepageIcon
-                width="34px"
-                onClick={() => alert('route homepage')}
-                className="cursor-pointer"
-            />
-            <ExploreIcon
-                width="34px"
-                onClick={() => alert('route explore page')}
-                className="cursor-pointer"
-            />
+            <Link href="/">
+                <HomepageIcon
+                    width="34px"
+                    onClick={() => alert('route homepage')}
+                    className="cursor-pointer"
+                />
+            </Link>
+            <Link href="/explore">
+                <ExploreIcon
+                    width="34px"
+                    onClick={() => alert('route explore page')}
+                    className="cursor-pointer"
+                />
+            </Link>
             <CreateIcon
                 width="34px"
                 onClick={() => alert('add point and share photo || share photo in any point')}
